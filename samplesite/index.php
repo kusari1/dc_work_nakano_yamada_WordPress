c557df59f18337b5a122bfda17e81dd997e5c226
HASHIYAMA <qqyc4rx9k@gmail.com>
2025-05-14 14:34:58 +0900

    wordpress レッスン7

--- samplesite/index.php
+++ samplesite/index.php
@@ -1,41 +1,3 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="UTF-8">
-<script type="text/javascript" src="js/jquery.js"></script>
-<script type="text/javascript" src="js/common.js"></script>
-<script type="text/javascript">
-$(function() {
-    $('#slide').slideshow({
-        autoSlide    : true,
-        effect       : 'fade',
-        type         : 'repeat',
-        interval     : 2000,
-        duration     : 500,
-        imgHoverStop : true,
-        navHoverStop : true
-    });
-});
-</script>
-<title>SAMPLE SITE</title>
- <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <header>
-        <div class="container">
-            <h1><a href="index.html"><img src="images/logo.png" alt="SAMPLE WEB SITE" /></a></h1>
-            <div id="menu">
-                <ul>
-                    <li class="home"><a href="index.html">TOP</a></li>
-                    <li><a href="company.html">会社概要</a></li>
-                    <li><a href="service.html">サービス概要</a></li>
-                    <li><a href="archive-news.html">プレスリリース</a></li>
-                    <li><a href="archive-news.html">ブログ</a></li>
-                    <li><a href="contact.html">お問い合わせ</a></li>
-                </ul>
-            </div>
-        </div>
-    </header>
     <div id="slide">
         <ul class="slide-inner">
             <li></li>
@@ -120,31 +82,5 @@ $(function() {
                     </dl>
                 </div>
             </div>       
-            <div id="cont_right">
-                <div class="sub-menu">
-                    <h3>SERVICE</h3>
-                    <h4>Web事業</h4>
-                    <ul>
-                        <li><a href="service.html#s1">ホームページ制作</a></li>
-                        <li><a href="service.html#s2">エンジニア派遣</a></li>
-                    </ul>
-                </div>
-            </div>
         </div>
     </div>
-    <footer>
-        <ul>
-            <li class="home"><a href="index.html">TOP</a></li>
-            <li><a href="service.html">SERVICE</a></li>
-            <li><a href="company.html">COMPANY</a></li>
-            <li><a href="contact.html">CONTACT</a></li>
-        </ul>
-        <div id="footer">
-            <div class="copyright">Copyright &copy; 2020 SAMPLE SITE All Rights Reserved.</div>
-        </div>
-    </footer>
-    <div id="pageTop">
-        <a href="#">PAGE TOP</a>
-    </div>
-</body>
-</html>
