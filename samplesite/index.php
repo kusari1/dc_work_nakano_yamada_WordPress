b1d190e64e1b2b688149cd5671727fb462c13b31
morihito1 <148161041+morihito1@users.noreply.github.com>
2025-05-20 13:10:55 +0900

    wordpress 最終課題の修正

--- samplesite/index.php
+++ samplesite/index.php
@@ -5,9 +5,9 @@
             <li></li>
             <li></li>
         </ul>
-         <div class="s-prev"><img src="<?php echo get_template_directory_uri(); ?>/images/nav_prev.png" alt="前へ"></div>
-         <div class="s-next"><img src="<?php echo get_template_directory_uri(); ?>/images/nav_next.png" alt="次へ"></div>
-         <div class="cont-nav"></div>
+        <div class="s-prev"><img src="<?php echo get_template_directory_uri(); ?>/images/nav_prev.png" alt="前へ"></div>
+        <div class="s-next"><img src="<?php echo get_template_directory_uri(); ?>/images/nav_next.png" alt="次へ"></div>
+        <div class="cont-nav"></div>
     </div>
     <div class="container">
         <div id="contents">
@@ -20,7 +20,7 @@
                 </div>
                 <div class="top-navColumn top-navColumn_right">
                     <h2>COMPANY</h2>
-                    <div class="top-navPhoto"><a href="<?php echo home_url(); ?>/company/"><img src="<?php echo get_template_directory_uri(); ?>/images/photo03.jpg" alt=""  width="460" /></a></div>
+                    <div class="top-navPhoto"><a href="<?php echo home_url(); ?>/company/"><img src="<?php echo get_template_directory_uri(); ?>/images/photo03.jpg" alt="" width="460" /></a></div>
                     <p>会社情報をご紹介します。</p>
                     <p class="top-navDetail"><a href="<?php echo home_url(); ?>/company/">MORE</a></p>
                 </div>
@@ -29,61 +29,56 @@
                 <div class="information">
                     <h2>INFORMATION</h2>
                     <dl>
-                        <dt>2020-08-01</dt>
+                        <?php $infoPosts = get_posts('numberposts=4&category=3,4,5'); foreach ($infoPosts as $post): ?>
+                        <dt><?php the_time('Y-m-d'); ?></dt>
                         <dd>
-                        <span class="tab tag_gyoumu">お知らせ</span>
-                        <a href="<?php echo home_url(); ?>/single/">夏季休暇のお知らせを掲載しました</a></dd>
-                        <dt>2020-07-29</dt>
-                        <dd>
-    					<span class="tab tag_release">プレスリリース</span>
-                        <a href="<?php echo home_url(); ?>/sample/">新サービスのお知らせを掲載しました</a></dd>
-                        <dt>2020-07-23</dt>
-                        <dd>
-    					<span class="tab tag_gyoumu">お知らせ</span>
-                        <a href="<?php echo home_url(); ?>/sample/">雑誌掲載情報を掲載しました</a></dd>
-                        <dt>2020-07-20</dt>
-                        <dd>
-                        <span class="tab tag_gyoumu">お知らせ</span>
-                        <a href="<?php echo home_url(); ?>/sample/">雑誌掲載情報を掲載しました</a></dd>
+                            <span class="tab tag_<?php $cat = get_the_category(); $cat = $cat[0]; { echo $cat->slug; } ?>"> 
+                            <?php $cat = get_the_category(); $cat = $cat[0]; { echo $cat->cat_name; } ?> </span>
+                            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>を掲載しました。</dd>
+                        </dd>
+
+                        <?php endforeach; ?>
                     </dl>
                 </div>
-
 
                 <div class="information">
                     <h2>BLOG</h2>
                     <dl>
                         <dt>2020-08-04</dt>
                         <dd>
-                        <div class="b_img">
-    	                    <img src="<?php echo get_template_directory_uri(); ?>/images/sample.jpg">
-                        </div>
-                         <div class="b_right">
-                        <a href="<?php echo home_url(); ?>/sample/">社長通信</a>
-                         </div></dd>
+                            <div class="b_img">
+                                <img src="<?php echo get_template_directory_uri(); ?>/images/sample.jpg">
+                            </div>
+                            <div class="b_right">
+                                <a href="<?php echo home_url(); ?>/sample/">社長通信</a>
+                            </div>
+                        </dd>
 
                         <dt>2020-08-02</dt>
                         <dd>
-                        <div class="b_img">
-    	                    <img src="<?php echo get_template_directory_uri(); ?>/images/sample.jpg">
-                        </div>
-                         <div class="b_right">
-                        <a href="<?php echo home_url(); ?>/sampl">社員紹介</a>
-                         </div></dd>
+                            <div class="b_img">
+                                <img src="<?php echo get_template_directory_uri(); ?>/images/sample.jpg">
+                            </div>
+                            <div class="b_right">
+                                <a href="<?php echo home_url(); ?>/sampl">社員紹介</a>
+                            </div>
+                        </dd>
 
                         <dt>2020-08-01</dt>
                         <dd>
-                        <div class="b_img">
-    	                    <img src="<?php echo get_template_directory_uri(); ?>/images/sample.jpg">
-                        </div>
-                         <div class="b_right">
-                        <a href="sample.html">セミナー開催報告</a>
-                         </div></dd>
+                            <div class="b_img">
+                                <img src="<?php echo get_template_directory_uri(); ?>/images/sample.jpg">
+                            </div>
+                            <div class="b_right">
+                                <a href="sample.html">セミナー開催報告</a>
+                            </div>
+                        </dd>
 
 
                     </dl>
                 </div>
-            </div>  
-            <?php get_sidebar(); ?>     
+            </div>
+            <?php get_sidebar(); ?>
         </div>
     </div>
     <?php get_footer(); ?>
